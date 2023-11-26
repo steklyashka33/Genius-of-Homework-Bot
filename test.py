de910984485a122bff1003eae96811d53facba87
steklyashka33 <troll16000@mail.ru>
2023-11-26 20:24:38 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,17 +1,24 @@
 import asyncio
-import logging, sys
-from typing import Dict, Any
+import logging
+import os
 
+from aiogram import Bot, Dispatcher, F
+from aiogram.enums import ContentType
 from aiogram.filters import CommandStart
 from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.storage.memory import MemoryStorage, SimpleEventIsolation
+from aiogram.types import Message, CallbackQuery
 
-from aiogram import Router, F, Bot, Dispatcher
-from aiogram.types import Message, User
-
-from aiogram_dialog import Dialog, Window, setup_dialogs, DialogManager
-from aiogram_dialog.widgets.text import Format, Const
-from aiogram_dialog.widgets.kbd import Checkbox, Button, Row, Cancel, Start
-from aiogram_dialog.widgets.input.text import TextInput
+from aiogram_dialog import (
+    Dialog, Window, DialogManager, StartMode, setup_dialogs,
+)
+from aiogram_dialog.api.entities import MediaAttachment, MediaId
+from aiogram_dialog.widgets.common import ManagedScroll
+from aiogram_dialog.widgets.input import MessageInput
+from aiogram_dialog.widgets.kbd import StubScroll, NumberedPager, Group, Button
+from aiogram_dialog.widgets.markup.reply_keyboard import ReplyKeyboardFactory
+from aiogram_dialog.widgets.media import DynamicMedia
+from aiogram_dialog.widgets.text import Const, Format
 
 
 from dotenv import load_dotenv
@@ -22,107 +29,83 @@ load_dotenv()
 # Bot token can be obtained via https://t.me/BotFather
 TOKEN = getenv("BOT_TOKEN")
 
-class MainMenu(StatesGroup):
-    START = State()
+
+class Medias(StatesGroup):
+    start = State()
 
 
-class Settings(StatesGroup):
-    START = State()
+async def on_input_photo(message: Message, widget: MessageInput,
+                         dialog_manager: DialogManager):
+    dialog_manager.dialog_data.setdefault("photos", []).append(
+        (message.photo[-1].file_id, message.photo[-1].file_unique_id),
+    )
 
 
-EXTEND_BTN_ID = "extend"
-CLASS_TEXTINPUT_ID = "class"
+async def on_delete(
+        callback: CallbackQuery, widget: Button, dialog_manager: DialogManager,
+):
+    scroll: ManagedScroll = dialog_manager.find("pages")
+    media_number = await scroll.get_page()
+    photos = dialog_manager.dialog_data.get("photos", [])
+    del photos[media_number]
+    if media_number > 0:
+        await scroll.set_page(media_number - 1)
 
 
-async def getter(dialog_manager: DialogManager, event_from_user: User, **kwargs) -> Dict[str, Any]:
-    if dialog_manager.find(EXTEND_BTN_ID).is_checked():
-        return {
-            "name": event_from_user.first_name,
-            "extended_str": "on",
-            "extended": True,
-        }
+async def getter(dialog_manager: DialogManager, **kwargs) -> dict:
+    scroll: ManagedScroll = dialog_manager.find("pages")
+    media_number = await scroll.get_page()
+    photos = dialog_manager.dialog_data.get("photos", [])
+    if photos:
+        photo = photos[media_number]
+        media = MediaAttachment(
+            file_id=MediaId(*photo),
+            type=ContentType.PHOTO,
+        )
     else:
-        return {
-            "name": event_from_user.first_name,
-            "extended_str": "off",
-            "extended": False,
-        }
-
-async def class_textinput_filter(*args, **kwargs):
-    args, kwargs
-    return True
-
-
-main_menu = Dialog(
-    Window(
-        Format(
-            "Hello, {name}. \n\n"
-            "Extended mode is {extended_str}.\n"
-        ),
-        Const(
-            "Here is some additional text, which is visible only in extended mode",
-            when="extended",
-        ),
-        Row(
-            Checkbox(
-                checked_text=Const("[x] Extended mode."),
-                unchecked_text=Const("[ ] Extended mode"),
-                id=EXTEND_BTN_ID,
-            ),
-            Start(Const("Settings"), id="settings", state=Settings.START),
-        ),
-        TextInput(
-            id=CLASS_TEXTINPUT_ID,
-            filter=class_textinput_filter,
-        ),
-        getter=getter,
-        state=MainMenu.START
-    )
-)
-
-NOTIFICATIONS_BTN_ID = "notify"
-ADULT_BTN_ID = "adult"
-
-settings = Dialog(
-    Window(
-        Const("Settings"),
-        Checkbox(
-            checked_text=Const("[x] Send notifications"),
-            unchecked_text=Const("[ ] Send notifications"),
-            id=NOTIFICATIONS_BTN_ID,
-        ),
-        Checkbox(
-            checked_text=Const("[x] Adult mode"),
-            unchecked_text=Const("[ ] Adult mode"),
-            id=ADULT_BTN_ID,
-        ),
-        Row(
-            Cancel(),
-            Cancel(text=Const("Save"), id="save"),
-        ),
-        state=Settings.START,
-    )
-)
-
-router = Router()
+        media = MediaAttachment(
+            url="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Image_not_available.png/800px-Image_not_available.png?20210219185637",
+            type=ContentType.PHOTO,
+        )
+    return {
+        "media_count": len(photos),
+        "media_number": media_number + 1,
+        "media": media,
+    }
 
 
-@router.message(CommandStart())
+dialog = Dialog(Window(
+    Const("Send media"),
+    DynamicMedia(selector="media"),
+    StubScroll(id="pages", pages="media_count"),
+    Group(
+        NumberedPager(scroll="pages", when=F["pages"] > 1),
+        width=8
+    ),
+    Button(Format("🗑️ Delete photo #{media_number}"), id="del",
+           on_click=on_delete, when="media_count"),
+    MessageInput(content_types=[ContentType.PHOTO], func=on_input_photo),
+    getter=getter,
+    state=Medias.start,
+))
+
+
 async def start(message: Message, dialog_manager: DialogManager):
-    await dialog_manager.start(MainMenu.START)
+    await dialog_manager.start(Medias.start, mode=StartMode.RESET_STACK)
 
 
 async def main():
+    # real main
+    logging.basicConfig(level=logging.INFO)
+    storage = MemoryStorage()
     bot = Bot(token=TOKEN)
-    dp = Dispatcher()
-    dp.include_router(main_menu)
-    dp.include_router(settings)
-    dp.include_router(router)
-    setup_dialogs(dp)
+    dp = Dispatcher(storage=storage, events_isolation=SimpleEventIsolation())
+    dp.include_router(dialog)
 
+    dp.message.register(start, CommandStart())
+    setup_dialogs(dp)
     await dp.start_polling(bot)
 
 
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+if __name__ == '__main__':
     asyncio.run(main())
